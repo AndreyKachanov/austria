a8e975c2bffe84f3639b7914b2ad9fa0c4350a9b
Andrey Kachanov <andrey170749@yandex.ru>
2015-04-21 18:18:37 +0300

    Add logo

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Моя австрия</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<link rel="stylesheet" type="text/css" href="style.css">	
+</head>
+<body>
+	<div id="wrapper">
+		<header>
+			<a href="#"><img src="images/logo.png" alt="Австрия" title="Австрия"></a>
+		</header>
+	</div>
+</body>
+</html>
