215efcf8958c07442daebff13c35fcb4a8941626
Andrey Kachanov <andrey170749@yandex.ru>
2015-04-24 21:39:24 +0300

    Added phone & work hours

--- index.php
+++ index.php
@@ -6,16 +6,30 @@
 	<link rel="stylesheet" type="text/css" href="style.css">	
 </head>
 <body>
-	<div id="wrapper">
+	<div id="wrapper" class="vcard">
 		<header>
-			<h1 class="logo">
-				<a href="index.php">MojaAustria</a>								
+			<h1 class="logotip">
+				<a class="fn org" href="index.php">Moja Austria</a>								
 			</h1>
 			<form class="form-wrapper">
 				<input type="text" id="search" placeholder="Поиск по сайту" required>
 				<input type="submit" value="" id="submit">				
 			</form>
 		</header>
+		<div id="content_slider">
+			<div id="slider">
+				<img src="images/slides/slide.jpg" alt="">
+			</div>
+			<div id="info">
+				<p class="phone">Телефон</p>
+				<p class="tel">+356 (0)6 601 530</p>
+				<div class="workhours">
+					<p>Время работы:</p>				
+					<p>пн-пт с 9.00 до 20.00</p>
+					<p><span>сб-вс</span> с 9.00 до 18.00</p>
+				</div>				
+			</div>			
+		</div>
 	</div>
 </body>
 </html>
